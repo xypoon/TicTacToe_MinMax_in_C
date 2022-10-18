46d7fe2bd86ca385006c76a44f859e8efeda79dc
xypoon <xiangyuanpoon@gmail.com>
2022-10-18 20:38:46 +0800

    menu done, linked to pvp

--- tictactoe.c
+++ tictactoe.c
@@ -1,12 +1,14 @@
 // Compile Code
 // As you add new SDL2 libraries, add their dll name to the compile command
-// gcc -I src/include -L src/lib -o main tictactoe.c -lmingw32 -lSDL2main -lSDL2
+// gcc -I src/include -L src/lib -o main tictactoe.c -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 
 //  Import Libraries
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 // Declare Variables
 #define N 3
@@ -37,6 +39,7 @@ typedef struct
 // Declare Functions
 void playerMove(GameState *game, int row, int column);
 void draw_in_terminal(int b[9]);
+void select_gamemode(int col);
 
 void DrawCircle(SDL_Renderer *renderer, int centreX, int centreY)
 {
@@ -225,12 +228,76 @@ int processEvents(SDL_Renderer *renderer, SDL_Window *window, GameState *game)
       {
         reset_board(game);
       }
-
       break;
     }
     }
   }
   return done;
+}
+
+int processMenuEvents(SDL_Renderer *renderer, SDL_Window *window)
+{
+  // SDL Event Listener
+  SDL_Event event;
+  int done = 0;
+  int menu = 0;
+
+  while (SDL_PollEvent(&event))
+  {
+    switch (event.type)
+    {
+    // Quiting Game
+    case SDL_WINDOWEVENT_CLOSE:
+    {
+      if (window)
+      {
+        printf("close Pressed\n");
+        SDL_DestroyWindow(window);
+        window = NULL;
+        menu = 1;
+      }
+    }
+    break;
+    case SDL_KEYDOWN:
+    {
+      switch (event.key.keysym.sym)
+      {
+      case SDLK_ESCAPE:
+        printf("Escape Pressed\n");
+        menu = 1;
+        break;
+      }
+    }
+    break;
+    case SDL_QUIT:
+      // quit out of the game
+      menu = 1;
+      break;
+    case SDL_MOUSEBUTTONDOWN:
+    {
+      select_gamemode(event.button.y / CELL_HEIGHT);
+      menu = 1;
+      break;
+    }
+    }
+  }
+  return menu;
+}
+
+void select_gamemode(int col)
+{
+  switch (col)
+  {
+  case 0:
+    printf("Player vs Player Selected\n");
+    break;
+  case 1:
+    printf("Player vs AI (Easy) Selected\n");
+    break;
+  case 2:
+    printf("Player vs AI (Impossible) Selected\n");
+    break;
+  }
 }
 
 void playerMove(GameState *game, int row, int column)
@@ -300,6 +367,7 @@ int main(int argc, char *argv[])
   SDL_Renderer *renderer; // Declare a renderer
 
   SDL_Init(SDL_INIT_VIDEO); // Initialise SDL2
+  TTF_Init();
 
   // Create an application window with the following settings
   window = SDL_CreateWindow(
@@ -324,8 +392,86 @@ int main(int argc, char *argv[])
 
   // Event Loop
   int done = 0;
+  int menu = 0;
   while (!done)
   {
+    if (menu == 0)
+    {
+      // set background black
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+      SDL_RenderClear(renderer);
+      // set text colour white
+      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+      for (int i = 0; i < N; i++)
+      {
+        SDL_RenderDrawLine(renderer,
+                           0, i * CELL_HEIGHT,
+                           SCREEN_WIDTH, i * CELL_HEIGHT);
+      }
+      // render text
+      TTF_Init();
+      TTF_Font *Sans = TTF_OpenFont("OpenSans-Regular.ttf", 24);
+      if (Sans == NULL)
+      {
+        fprintf(stderr, "error: font not found\n");
+        exit(EXIT_FAILURE);
+      }
+      SDL_Color White = {255, 255, 255};
+      // as TTF_RenderText_Solid could only be used on
+      // SDL_Surface then you have to create the surface first
+      SDL_Surface *menu1 =
+          TTF_RenderText_Solid(Sans, "Player Vs Player", White);
+      SDL_Surface *menu2 =
+          TTF_RenderText_Solid(Sans, "Player Vs AI (Easy)", White);
+      SDL_Surface *menu3 =
+          TTF_RenderText_Solid(Sans, "Player Vs AI (Impossible)", White);
+      // now you can convert it into a texture
+      SDL_Texture *menu1Texture = SDL_CreateTextureFromSurface(renderer, menu1);
+      SDL_Texture *menu2Texture = SDL_CreateTextureFromSurface(renderer, menu2);
+      SDL_Texture *menu3Texture = SDL_CreateTextureFromSurface(renderer, menu3);
+
+      SDL_Rect Menu1_rect;                  // create a rect
+      Menu1_rect.x = SCREEN_WIDTH / 3 - 25; // controls the rect's x coordinate
+      Menu1_rect.y = 20;                    // controls the rect's y coordinte
+      Menu1_rect.w = 250;                   // controls the width of the rect
+      Menu1_rect.h = 100;                   // controls the height of the rect
+
+      SDL_Rect Menu2_rect;                  // create a rect
+      Menu2_rect.x = SCREEN_WIDTH / 3 - 25; // controls the rect's x coordinate
+      Menu2_rect.y = CELL_HEIGHT + 20;      // controls the rect's y coordinte
+      Menu2_rect.w = 250;                   // controls the width of the rect
+      Menu2_rect.h = 100;                   // controls the height of the rect
+
+      SDL_Rect Menu3_rect;                  // create a rect
+      Menu3_rect.x = SCREEN_WIDTH / 3 - 50; // controls the rect's x coordinate
+      Menu3_rect.y = CELL_HEIGHT * 2 + 20;  // controls the rect's y coordinte
+      Menu3_rect.w = 300;                   // controls the width of the rect
+      Menu3_rect.h = 100;                   // controls the height of the rect
+
+      SDL_RenderCopy(renderer, menu1Texture, NULL, &Menu1_rect);
+      SDL_RenderCopy(renderer, menu2Texture, NULL, &Menu2_rect);
+      SDL_RenderCopy(renderer, menu3Texture, NULL, &Menu3_rect);
+
+      SDL_RenderPresent(renderer);
+
+      // Don't forget to free your surface and texture
+      SDL_FreeSurface(menu1);
+      SDL_DestroyTexture(menu1Texture);
+      SDL_FreeSurface(menu2);
+      SDL_DestroyTexture(menu2Texture);
+      SDL_FreeSurface(menu3);
+      SDL_DestroyTexture(menu3Texture);
+
+      while (menu == 0)
+      {
+
+        menu = processMenuEvents(renderer, window);
+
+        // Delay Refresh so as too not overun
+        SDL_Delay(100);
+      }
+    }
+
     // Render Display Grid for Tic Tac Toe
     render_grid(renderer, &gameState);
     render_board(renderer,
